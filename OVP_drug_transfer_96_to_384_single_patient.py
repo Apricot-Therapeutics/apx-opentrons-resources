4f97f481babb99879fad9b602cc3ac93e1c97fa2
adrtsc <aditschans@gmail.com>
2024-06-06 11:06:21 +0200

    Fix bug in pipette loading

--- OVP_drug_transfer_96_to_384_single_patient.py
+++ OVP_drug_transfer_96_to_384_single_patient.py
@@ -22,6 +22,10 @@ def run(protocol: protocol_api.ProtocolContext):
     drug_plate = protocol.load_labware("thermoscientific_96_wellplate_1300ul", 5)
     cell_plate = protocol.load_labware("greiner_bio_one_384_well_plate_100ul_reduced_well_size", 6)
 
+    # for local testing
+    #drug_plate = protocol.load_labware("nest_96_wellplate_200ul_flat", 2)
+    #cell_plate = protocol.load_labware("corning_384_wellplate_112ul_flat", 3)
+
     # optional: set liquids
     sample = protocol.define_liquid(name="sample", display_color="#1c03fc",
                                     description="sample to which to add drugs")
@@ -31,36 +35,36 @@ def run(protocol: protocol_api.ProtocolContext):
     # load some metadata we need later
 
     # load the drug layout on drug master plate and final 384-well plate
-    drug_layout_96 = pd.read_csv(
-        r"K:\projects\OV_Precision\documents\plate_layout\drug_plate_metadata_v1.0.csv")
-    drug_layout_384 = pd.read_csv(
-        r"K:\projects\OV_Precision\documents\plate_layout\plate_metadata_v1.0.csv")
+    drug_plate_layout = pd.read_csv(
+        r"C:\Users\OT-Operator\Documents\OT-2_protocols\Apricot\OVP\metadata\drug_plate_metadata_v1.0.csv")
+    cell_plate_layout = pd.read_csv(
+        r"C:\Users\OT-Operator\Documents\OT-2_protocols\Apricot\OVP\metadata\plate_metadata_v1.0.csv")
 
     # for now, only 1 patient
-    drug_layout_384 = drug_layout_384.loc[
-        drug_layout_384["sample"] != "patient_2"]
+    cell_plate_layout = cell_plate_layout.loc[
+        cell_plate_layout["sample"] != "patient_2"]
 
     # load drugs into 96-well plate
-    for i, well in drug_layout.iterrows():
+    for i, well in drug_plate_layout.iterrows():
         well = drug_plate[well.row + str(well.col)]
         well.load_liquid(liquid=drugs, volume=1000)
 
-    for i, well in drug_layout_384.iterrows():
+    for i, well in cell_plate_layout.iterrows():
         well = cell_plate[well.row + str(well.col)]
         well.load_liquid(liquid=sample, volume=45)
 
     # initialize pipette
     left_pipette = protocol.load_instrument("p300_multi_gen2", "left",
                                             tip_racks=[tips])
-    right_pipette = protocol.load_instrument("p20_single_gen2", "left",
+    right_pipette = protocol.load_instrument("p20_single_gen2", "right",
                                             tip_racks=[tips])
 
-    for i, drug in drug_layout_96.iterrows():
+    for i, drug in drug_plate_layout.iterrows():
         # assemble name of source well (opentrons take A1 instead of A01)
         source_well = drug.row + str(drug.col)
         # collect destination wells
-        dest_wells = drug_layout_384.loc[
-            drug_layout_384.condition == drug.condition]
+        dest_wells = cell_plate_layout.loc[
+            cell_plate_layout.condition == drug.condition]
         # put together names for destination wells
         dest_wells = [well.row + str(well.col) for i, well
                       in dest_wells.iterrows()]
